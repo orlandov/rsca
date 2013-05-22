1a4f14ec674a3b0eed369233d802951812abfad7
Orlando Vazquez <ovazquez@gmail.com>
2013-05-21 15:40:39 -1000

    Break out formatting into its own functions

--- tests/test-formatters.js
+++ tests/test-formatters.js
@@ -1,22 +1,79 @@
 var assert = require('assert');
 var formatters = require('../lib/formatters');
 
-describe('config block', function (done) {
-    it('formats a config block', function (done) {
-        var actual = formatters.block({
-            name: 'Mission'
-        });
-        var expected = [
-            "class Mission",
-            "{",
-            "};"
-        ].join("\n");
+describe('formatters.formatValue', function (done) {
+    var value, actual, expected;
+
+    it('formats a string', function (done) {
+        value = 'hello world';
+        expected = '"hello world"';
+        actual = formatters.formatValue(value);
+
+        assert.equal(actual, expected);
+
+        value = 'hello "cruel" world';
+        expected = '"hello ""cruel"" world"';
+        actual = formatters.formatValue(value);
+
+        assert.equal(actual, expected);
+
+        done();
+    });
+
+    it('formats a number', function (done) {
+        value = 123;
+        expected = "123";
+        actual = formatters.formatValue(value);
+
+        assert.equal(actual, expected);
+
+        value = 123.4;
+        expected = "123.4";
+        actual = formatters.formatValue(value);
 
         assert.equal(actual, expected);
         done();
     });
 
-    it('formats a nested block with indent', function (done) {
+    it('formats an expression', function (done) {
+        value = { type: 'expr', val: '_weather' };
+        expected = '@_weather';
+        actual = formatters.formatValue(value);
+
+        assert.equal(actual, expected);
+
+        value = {
+            type: 'expr',
+            val: 'format ["BIS_patrolLength = %1", _PatrolLength]'
+        };
+        expected = '@"format [""BIS_patrolLength = %1"", _PatrolLength]"';
+        actual = formatters.formatValue(value);
+
+        assert.equal(actual, expected);
+        done();
+    });
+});
+
+
+describe('formatters.block', function (done) {
+
+    it('formats a config block',
+    function (done) {
+        var actual = formatters.block({
+            name: 'Mission'
+        });
+        var expected = [
+            'class Mission',
+            '{',
+            '};'
+        ].join('\n');
+
+        assert.equal(actual, expected);
+        done();
+    });
+
+    it('formats a nested block with indent',
+    function (done) {
         var block1 = formatters.block({
             name: 'Intel',
             content: 'something'
@@ -28,43 +85,45 @@ describe('config block', function (done) {
         });
 
         var expected = [
-            "class Mission",
-            "{",
-            "    class Intel",
-            "    {",
-            "        something",
-            "    };",
-            "};"
-        ].join("\n");
+            'class Mission',
+            '{',
+            '    class Intel',
+            '    {',
+            '        something',
+            '    };',
+            '};'
+        ].join('\n');
 
         assert.equal(block0, expected);
         done();
     });
 
-    it('formats a config block with indent', function (done) {
+    it('formats a config block with indent',
+    function (done) {
         var actual = formatters.block({
             name: 'Mission',
             content: 'something'
         });
 
         var expected = [
-            "class Mission",
-            "{",
-            "    something",
-            "};"
-        ].join("\n");
+            'class Mission',
+            '{',
+            '    something',
+            '};'
+        ].join('\n');
 
         assert.equal(actual, expected);
         done();
     });
 
-    it('formats a config block that has key/value pairs', function (done) {
+    it('formats a config block that has array key/value pairs',
+    function (done) {
         var actual = formatters.block({
             name: 'Mission',
             content: 'something',
             values: {
-                addOns: ["addon0", "addon1"],
-                addOnsAuto: ["addon2", "addon3"]
+                addOns: ['addon0', 'addon1'],
+                addOnsAuto: ['addon2', 'addon3']
             }
         });
 
@@ -81,7 +140,111 @@ describe('config block', function (done) {
             '    };',
             '    something',
             '};'
-        ].join("\n");
+        ].join('\n');
+
+        assert.equal(actual, expected);
+        done();
+    });
+
+    it('formats a config block that has number key/value pairs',
+    function (done) {
+        var actual = formatters.block({
+            name: 'Mission',
+            content: 'something',
+            values: {
+                addOns: ['addon0', 'addon1'],
+                addOnsAuto: ['addon2', 'addon3'],
+                randomseed: 123.4
+            }
+        });
+
+        var expected = [
+            'class Mission',
+            '{',
+            '    addOns[] = {',
+            '        "addon0",',
+            '        "addon1"',
+            '    };',
+            '    addOnsAuto[] = {',
+            '        "addon2",',
+            '        "addon3"',
+            '    };',
+            '    randomseed = 123.4;',
+            '    something',
+            '};'
+        ].join('\n');
+
+        assert.equal(actual, expected);
+        done();
+    });
+
+    it('formats a config block that has string key/value pairs',
+    function (done) {
+        var actual = formatters.block({
+            name: 'Mission',
+            content: 'something',
+            values: {
+                expActiv: 'H1 land "GET IN"'
+            }
+        });
+
+        var expected = [
+            'class Mission',
+            '{',
+            '    expActiv = "H1 land ""GET IN""";',
+            '    something',
+            '};'
+        ].join('\n');
+
+        assert.equal(actual, expected);
+        done();
+    });
+
+    it('formats a config block that has "referency" key/value pairs',
+    function (done) {
+        var actual = formatters.block({
+            name: 'Mission',
+            content: 'something',
+            values: {
+                startWeather: {
+                    type: 'expr',
+                    val: '_weather'
+                }
+            }
+        });
+
+        var expected = [
+            'class Mission',
+            '{',
+            '    startWeather = @_weather;',
+            '    something',
+            '};'
+        ].join('\n');
+
+        assert.equal(actual, expected);
+        done();
+    });
+
+    it('formats a config block that has "quoted reference" key/value pairs',
+    function (done) {
+        var actual = formatters.block({
+            name: 'Mission',
+            content: 'something',
+            values: {
+                init: {
+                    type: 'expr',
+                    val: 'format ["BIS_patrolLength = %1", _PatrolLength]'
+                }
+            }
+        });
+
+        var expected = [
+            'class Mission',
+            '{',
+            '    init = @"format [""BIS_patrolLength = %1"", _PatrolLength]";',
+            '    something',
+            '};'
+        ].join('\n');
 
         assert.equal(actual, expected);
         done();
